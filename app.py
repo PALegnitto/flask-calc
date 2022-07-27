77832e5d2695f48d4d1ae3e26b20f75371bbb128
Phill Legnitto <palegnitto@gmail.com>
2022-07-27 12:35:26 -0700

    built flask app to respond to /add route

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add
+
+
+app = Flask(__name__)
+
+@app.get("/add")
+def add_numbers():
+    """adds a and b and returns result as body"""
+
+    a = request.args["a"]
+    b = request.args["b"]
+    return f"<body>{add(a,b)}</body>" 
+
